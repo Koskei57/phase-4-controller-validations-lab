719980026e5c25191acda0c2a516a3c5faa43845
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-09-27 16:40:36 +0300

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,28 @@
 class AuthorsController < ApplicationController
-  
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_new_author_invalid
+
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
 
   private
-  
+
   def author_params
     params.permit(:email, :name)
   end
-  
+
+  def render_new_author_invalid(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+
+  def render_not_found_response
+    render json: { errors: "a kind of Hash" }, status: :not_found
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates_inclusion_of :category, :in => %w(Fiction Non-Fiction)
+  validates :content, length: { minimum: 100 }
 end
