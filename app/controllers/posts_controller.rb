3b5a1cd0e5b7fc38fce1fe174254e752b4c69474
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-09-27 16:40:28 +0300

    commit tree 1f94cc21b572adc0339e9e5ef46d188460010b94

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,32 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_updated_post_invalid
 
   def show
     post = Post.find(params[:id])
-    
+
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
 
   private
 
+  def render_updated_post_invalid(invalid)
+    render json: { errors: invalid.record.errors}, status: :unprocessable_entity
+  end
+
+  def render_not_found_response
+    render json: { errors: "a kind of Hash" }, status: :not_found
+  end
+
   def post_params
     params.permit(:category, :content, :title)
   end
-
 end
